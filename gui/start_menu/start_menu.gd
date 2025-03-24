8c78fed2647987887e0b4edf1671e270b7e9d05d
Piotr Gawle <ptr.gawle@gmail.com>
2025-03-24 20:36:39 +0100

    add basic enemy, global debug

--- gui/start_menu/start_menu.gd
+++ gui/start_menu/start_menu.gd
@@ -3,20 +3,11 @@ extends Control
 signal on_start
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if(Globals.debug_mode):
+		visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_gui_input(event: InputEvent) -> void:
-	
-	pass # Replace with function body.
 
 
 func _on_button_pressed() -> void:
 	visible = false
-	#on_start.emit()
-	pass # Replace with function body.
